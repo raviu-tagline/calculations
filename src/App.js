f44034a80cfd5eaf1c9662740a503452df3f05d6
ravi <raviu.tagline@gmail.com>
2021-06-24 15:06:06 +0530

    Dynamic terminal - order to done by TL

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Calculations from "./components/Calculations";
 
--- src/components/Calculations.jsx
+++ src/components/Calculations.jsx
@@ -1,13 +1,73 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Terminal from "terminal-in-react";
 import moment from "moment";
 
 const Calculations = () => {
   const symbols = ["+", "-", "*", "%", "/"];
+  const tokens = [
+    "today",
+    "tomorrow",
+    "yesterday",
+    "days",
+    "week",
+    "year",
+    "month",
+  ];
   const hasOperator = (str) => {
-    symbols.some((val) => {
-      return str.indexOf(val) !== -1;
-    });
+    return symbols.some((val) => str.indexOf(val));
+  };
+
+  const hasTokens = (str) => {
+    return tokens.some((val) => str.indexOf(val));
+  };
+
+  const chkString = (str) => {
+    let tknIndex = hasTokens(str);
+    let oprtrIndex = hasOperator(str);
+
+    if (tknIndex && oprtrIndex) {
+      let date = moment();
+      let operator = str[str.indexOf("+")];
+      let num = str.match(/(\d+)/);
+      let unit = str.match(/days|years|months|day|year|month/g);
+      if (!operator && !unit) {
+        switch (str.toLowerCase()) {
+          case "today":
+            console.log(date.format("DD/MM/YYYY"));
+            break;
+          case "yesterday":
+            console.log(date.subtract(1, "day").format("DD/MM/YYYY"));
+            break;
+
+          case "tomorrow":
+            console.log(date.add(1, "day").format("DD/MM/YYYY"));
+            break;
+
+          case "time":
+            console.log(date.format("hh:mm"));
+            break;
+          default:
+            console.log(`default`, str);
+            break;
+        }
+      } else {
+        switch (operator) {
+          case "+":
+            console.log(date.add(num?.[0], unit?.[1]).format("DD/MM/YYYY"));
+            break;
+          case "-":
+            console.log(date.subtract(1, "day").format("DD/MM/YYYY"));
+            break;
+          default:
+            console.log(`default`, str);
+            break;
+        }
+      }
+    } else if (oprtrIndex) {
+      console.log(eval(str));
+    } else {
+      console.log(`Invalid expression`);
+    }
   };
 
   return (
@@ -25,23 +85,28 @@ const Calculations = () => {
         barColor="black"
         style={{ fontWeight: "bold", fontSize: "1em", marginRight: "10px" }}
         prompt="white"
-        commandPassThrough={(cmd, print) => {
+        commandPassThrough={(cmd) => {
           let comm = cmd.join("");
-          let index = hasOperator(comm);
-          if (index && index !== -1) {
-            console.log(eval(comm));
-          } else {
-            let date = moment();
-            if (comm.toLowerCase() === "today") {
-              console.log(date.format("DD-MM-YYYY"));
-            } else if (comm.toLowerCase() === "tomorrow") {
-              console.log(date.add(1, "day").format("DD-MM-YYYY"));
-            } else if (comm.toLowerCase() === "yesterday") {
-              console.log(date.subtract(1, "day").format("Do-MM-YYYY"));
-            } else {
-              // console.log(comm);
-            }
-          }
+          // let index = hasOperator(comm);
+          // let today = moment().format("DD/MM/YYYY");
+          // let yesterday = moment().subtract(1, "days").format("DD/MM/YYYY");
+          // let tomorrow = moment().add(1, "days").format("DD/MM/YYYY");
+
+          chkString(comm);
+
+          // if (index && index !== -1) {
+          //   console.log(eval(comm));
+          // } else {
+          //   if (comm.toLowerCase() === "today") {
+          //     console.log(today);
+          //   } else if (comm.toLowerCase() === "tomorrow") {
+          //     console.log(tomorrow);
+          //   } else if (comm.toLowerCase() === "yesterday") {
+          //     console.log(yesterday);
+          //   } else {
+          //     console.log(comm);
+          //   }
+          // }
         }}
       />
       <Terminal
